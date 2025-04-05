ede523b2ea3e00a5867af4951bfb03b78472d212
permlap-phola <permlap@tatugacamp.com>
2025-04-05 20:40:22 +0700

    Remove convert:tsx script out off build

--- unlayer.d.ts
+++ unlayer.d.ts
@@ -4,5 +4,13 @@ declare global {
   const unlayer: Unlayer;
   interface Window {
     __unlayer_lastEditorId: number;
+    unlayer: UnlayerType; // Assuming 'unlayer' is attached to the window
   }
+}
+
+interface UnlayerType {
+  registerPropertyEditor(data: any): void; // Correct definition!
+  createWidget(config: any): any; // Add other definitions as needed
+  registerTool(config: any): void;
+  createViewer(config: any): any;
 }
