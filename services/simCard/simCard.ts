a89a82cd8e9749baf5d28f6adbd7105ab9543b6b
permlap-phola <permlap@tatugacamp.com>
2026-01-28 16:58:02 +0700

    Change download format from XLSX to CSV in Generate IMEI Excel Service

--- services/simCard/simCard.ts
+++ services/simCard/simCard.ts
@@ -395,7 +395,7 @@ export async function GenerateImeiExcelService(): Promise<void> {
     const url = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement("a");
     link.href = url;
-    link.setAttribute("download", "imei_output.xlsx");
+    link.setAttribute("download", "imei_output.csv");
     document.body.appendChild(link);
     link.click();
     link.remove();
